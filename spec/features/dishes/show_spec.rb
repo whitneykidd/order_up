11ab3573d05c58a87af6db323db0842c46fe863d
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-29 09:26:14 -0600

    write story 1 test

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Dish Show Page" do
+  before(:each) do
+    @bert = Chef.create(name: "Bert")
+    @ernie = Chef.create(name: "Ernie")
+    @nachos =Dish.create(name: "Nacho Surprise", description: "It's a surprise", chef_id: @bert.id)
+    @chips = @nachos.ingredients.create(name: "Tortilla Chips", calories: 32)
+    @cheese = @nachos.ingredients.create(name: "Cheese", calories: 16)
+    @salsa = @nachos.ingredients.create(name: "Salsa", calories: 25)
+
+    visit "/dishes/#{@bert.id}"
+  end
+
+  it "can see dish's name, chef's name, and dish's ingredients" do
+    expect(current_path).to eq("/dishes/#{@bert.id}")
+    expect(page).to have_content(@nachos.name)
+    expect(page).to have_content(@chips.name)
+    expect(page).to have_content(@cheese.name)
+    expect(page).to have_content(@salsa.name)
+
+    expect(page).to have_content(@bert.name)
+    expect(page).to have_no_content(@ernie.name)
+
+  end
+end
+
+# Story 1 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see a list of ingredients for that dish
+# and the chef's name
